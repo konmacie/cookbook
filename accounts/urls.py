69c906bcc01a6f69158eddecd527e1599eee3dd6
Konrad Macieja <konrad.macieja@gmail.com>
2019-11-17 01:06:17 +0100

    added sign up view

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path, include
-
+from django.contrib.auth import views as auth_views
+from . import views
 
 urlpatterns = [
-    path('', include('django.contrib.auth.urls')),
+    # path('', include('django.contrib.auth.urls')),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('sign_up/', views.signup, name='signup'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
 
-# Create your views here.
+from .forms import SignUpForm
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # authenticating user after succsessful sign up
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('index')
+    else:
+        form = SignUpForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'registration/sign_up.html', context)
